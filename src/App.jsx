38237f82b04832bc404059f54f696298ccf1cd16
Rafael <rr65887@gmail.com>
2023-09-14 21:52:43 -0300

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react';
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+      <div className='main-box'>
+        <Counter />
+      </div>
+    </>
+  )
+}
+
+export default App
